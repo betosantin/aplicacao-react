9d3932faa69d445475adcf9a0cd8088adffe3973
Robertto Santin <betosantin@gmail.com>
2019-02-06 10:44:31 -0200

    corrigido variavel

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,4 +1,4 @@
-module.exports = function(req, rest, next){
+module.exports = function(req, res, next){
     res.header('Acess-Control-Allow-Origin', '*')
     res.header('Acess-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
